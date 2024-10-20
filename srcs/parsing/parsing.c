4d24dd8762ab025da0f68c76fa1f75581a9a36ff
Nabil080 <nbellila@student.42.fr>
2024-10-20 17:59:15 +0200

    parsing file for elements done, map is next

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -1,6 +1,6 @@
 #include "cub3d.h"
 
-void check_args(int argc, char **argv)
+int open_file(int argc, char **argv)
 {
 	int		error_code;
 	int		fd;
@@ -13,13 +13,46 @@ void check_args(int argc, char **argv)
 	if (argv[1][0] == '.')
 		exit_error(3);
 	fd = open(argv[1], O_RDONLY);
-	error_code = 0;
 	if (fd < 0)
-		error_code = 4;
-	else if (read(fd, 0, 0) < 0)
+		exit_error(4);
+	error_code = 0;
+	if (read(fd, 0, 0) < 0)
 		error_code = 5;
 	else if (read(fd, buffer, 1) < 1)
 		error_code = 6;
-	close (fd);
+	close(fd);
 	exit_error(error_code);
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		exit_error(4);
+	return (fd);
+}
+
+char	*get_value_by_identifier(int fd, const char *id, t_data *data)
+{
+	char	*value;
+	char	*line;
+
+	line = get_next_line(fd);
+	while (line && line[0] == '\n')
+	{
+		free(line);
+		line = get_next_line(fd);
+	}
+	if (line == NULL || ft_strncmp(id, line, ft_strlen(id)))
+	{
+		if (line)
+			free(line);
+		exit_free(7, data); // exit on wrong order or missing id;
+	}
+	if (line[ft_strlen(id) + 1] == '\0')
+	{
+		free(line);
+		exit_free(8, data); // exit on missing value;
+	}
+	value = ft_strdup(line + ft_strlen(id) + 1);
+	free(line);
+	if (!value)
+		exit_free(10, data); // malloc error
+	return (value);
 }
--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -1,13 +1,17 @@
 #include "cub3d.h"
 
-static const char	*error_messages[8] = {
-	"",
-	"Wrong number of arguments.\nUsage: ./cub3d <map.cub>",
-	"Wrong file extension.\nUsage: ./cub3d <map.cub>",
-	"No filename.\nUsage: ./cub3d <map.cub>",
-	"The file can't be opened.",
-	"The file cannot be read from.",
-	"The file is empty.",
+static const char	*error_messages[50] = {
+	"", // 0
+	"Wrong number of arguments.\nUsage: ./cub3d <map.cub>", // 1
+	"Wrong file extension.\nUsage: ./cub3d <map.cub>", // 2
+	"No filename.\nUsage: ./cub3d <map.cub>", // 3
+	"The file can't be opened.", // 4 
+	"The file cannot be read from.", // 5
+	"The file is empty.", // 6
+	"Wrong or missing identifier. (NO,SO,WE,EA,F,C)", // 7
+	"Missing value for an identifier.", // 8
+	"", // 9
+	"Allocation error.", // 10
 	NULL,
 };
 
@@ -18,3 +22,19 @@ void	exit_error(int code)
 	ft_putendl_fd(error_messages[code], 2);
 	exit(code);
 }
+
+void	exit_free(int code, t_data *data)
+{
+	exit_error(code);
+	free(data);
+}
+
+void	show_data(const t_data data)
+{
+	printf("N_texture: %s\n", data.N_texture);
+	printf("S_texture: %s\n", data.S_texture);
+	printf("W_texture: %s\n", data.W_texture);
+	printf("E_texture: %s\n", data.E_texture);
+	printf("F_color: %s\n", data.F_color);
+	printf("C_color: %s\n", data.C_color);
+};
