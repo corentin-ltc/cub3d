718335949ffb2b616f70eda641ad3e298d44c8fb
Nabil080 <bellilanabil8@gmail.com>
2024-10-20 21:25:13 +0200

    free data

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -28,72 +28,72 @@ int open_file(int argc, char **argv)
 		exit_error(ERR_FILE_OPEN);
 	return (fd);
 }
+
+static void	get_element(char **elem, int fd, t_data *data)
+{
+	size_t	start;
+	size_t	i;
+
+	i = 0;
+	while (ft_isalpha(data->tmp[i]))
+		i++;
+	while (ft_isspace(data->tmp[i]))
+		i++;
+	if (data->tmp[i] == '\0')
+		exit_free(ERR_VALUE_MISSING, data);
+	start = i;
+	while (data->tmp[i] && data->tmp[i] != '\n' && !ft_isspace(data->tmp[i]))
+		i++;
+	while (ft_isspace(data->tmp[i]))
+		i++;
+	if (data->tmp[i] != '\0')
+		exit_free(ERR_VALUE_ERROR, data);
+	*elem = ft_strdup(data->tmp);
+	return ;
+	fd++;
+	data++;
+}
+
+void	get_elements(int fd, t_data *data)
+{
+	while (!data->N_texture || !data->S_texture || !data->E_texture
+		|| !data->W_texture || !data->F_color || !data->C_color)
+	{
+		data->tmp = skip_empty_lines(fd);
+		if (data->tmp == NULL)
+			exit_free(ERR_ID_MISSING, data);
+		if (ft_strncmp(data->tmp, "NO", 2) == 0)
+			get_element(&data->N_texture, fd, data);
+		else if (ft_strncmp(data->tmp, "SO", 2) == 0)
+			get_element(&data->S_texture, fd, data);
+		else if (ft_strncmp(data->tmp, "WE", 2) == 0)
+			get_element(&data->W_texture, fd, data);
+		else if (ft_strncmp(data->tmp, "EA", 2) == 0)
+			get_element(&data->E_texture, fd, data);
+		else if (ft_strncmp(data->tmp, "F", 1) == 0)
+			get_element(&data->F_color, fd, data);
+		else if (ft_strncmp(data->tmp, "C", 1) == 0)
+			get_element(&data->C_color, fd, data);
+		else
+			exit_free(ERR_ID_MISSING, data);
+		free(data->tmp);
+		data->tmp = NULL;
+	}
+	free(data->tmp);
+	data->tmp = NULL;
+}
+
 /*Returns the value found on the identifier line*/
 /*Puts the fd cursor after the correct line*/
 /*Exits the program on error*/
-char	*get_value_by_identifier(int fd, const char *id, t_data *data)
-{
-	char	*value;
-
-	data->tmp = get_next_line(fd);
-	while (data->tmp && data->tmp[0] == '\n')
-	{
-		free(data->tmp);
-		data->tmp = get_next_line(fd);
-	}
-	if (data->tmp == NULL || ft_strncmp(id, data->tmp, ft_strlen(id)))
-		exit_free(ERR_ID_MISSING, data);
-	if (data->tmp[ft_strlen(id) + 1] == '\0')
-		exit_free(ERR_VALUE_MISSING, data);
-	value = ft_strdup(data->tmp + ft_strlen(id));
-	free(data->tmp);
-	data->tmp = NULL;
-	if (!value)
-		exit_free(ERR_MALLOC, data);
-	return (value);
-}
-
-static bool	is_valid_line(char *line)
-{
-	size_t	i;
-	bool	is_valid;
-
-	i = 0;
-	is_valid = true;
-	while (line[i] && is_valid)
-	{
-		is_valid = false;
-		if (ft_isspace(line[i]) || line[i] == '\n')
-			is_valid = true;
-		if (line[i] == '0' || line[i] == '1')
-			is_valid = true;
-		if (line[i] == 'N' || line[i] == 'S' || line[i] == 'E' || line[i] == 'W')
-			is_valid = true;
-		i++;
-	}
-	return (is_valid);
-}
-
-char	**get_map(int fd, t_data *data)
-{
-	char	**map;
-	t_list	*lines;
-
-	map = NULL;
-	data->tmp = get_next_line(fd);
-	while  (data->tmp && data->tmp[0] == '\n')
-	{
-		free(data->tmp);
-		data->tmp = get_next_line(fd);
-	}
-	if (data->tmp == NULL)
-		exit_free(ERR_MAP_MISSING, data);
-	lines = NULL;
-	while (data->tmp)
-	{
-		if (is_valid_line(data->tmp) == false || data->tmp[0] == '\n')
-			exit_free(ERR_MAP_CHAR, data);
-		data->tmp = get_next_line(fd);
-	}
-	return (map);
-}
+// char	*get_value_by_identifier(int fd, const char *id, t_data *data)
+// {
+// 	if (data->tmp[ft_strlen(id) + 1] == '\0')
+// 		exit_free(ERR_VALUE_MISSING, data);
+// 	value = ft_strdup(data->tmp + ft_strlen(id));
+// 	free(data->tmp);
+// 	data->tmp = NULL;
+// 	if (!value)
+// 		exit_free(ERR_MALLOC, data);
+// 	return (value);
+// }
