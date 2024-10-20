e52d35ab63222d7d15d8198754a88bf4eca8d94d
Nabil080 <bellilanabil8@gmail.com>
2024-10-20 16:36:23 +0200

    static global g_error_messages

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -1,20 +1,20 @@
 #include "cub3d.h"
 
+static const char	*error_messages[8] = {
+	"",
+	"Wrong number of arguments.\nUsage: ./cub3d <map.cub>",
+	"Wrong file extension.\nUsage: ./cub3d <map.cub>",
+	"No filename.\nUsage: ./cub3d <map.cub>",
+	"The file can't be opened.",
+	"The file cannot be read from.",
+	"The file is empty.",
+	NULL,
+};
+
 void	exit_error(int code)
 {
 	if (code == 0)
 		return ;
-	if (code == 1)
-		ft_putendl_fd("Wrong number of arguments.\nUsage: ./cub3d <map.cub>", 2);
-	else if (code == 2)
-		ft_putendl_fd("Wrong file extension.\nUsage: ./cub3d <map.cub>", 2);
-	else if (code == 3)
-		ft_putendl_fd("No filename.\nUsage: ./cub3d <map.cub>", 2);
-	else if (code == 4)
-		ft_putendl_fd("The file can't be opened.", 2);
-	else if (code == 5)
-		ft_putendl_fd("The file cannot be read from.", 2);
-	else if (code == 6)
-		ft_putendl_fd("The file is empty.", 2);
+	ft_putendl_fd(error_messages[code], 2);
 	exit(code);
 }
