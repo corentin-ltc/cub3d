af1c9e583bd4ad87d3898e1ba5e169ef6136bac6
Nabil080 <bellilanabil8@gmail.com>
2024-10-20 15:27:08 +0200

    parsing base

---
+++ includes/cub3d.h
@@ -0,0 +1,8 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "libft.h"
+
+bool check_args(int argc, char **argv);
+
+#endif
---
+++ srcs/parsing/parsing.c
@@ -0,0 +1,8 @@
+#include "cub3d.h"
+
+bool check_args(int argc, char **argv)
+{
+	if (argc && argv)
+		return (true);
+	return (false);
+}
