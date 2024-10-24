2b0276b86d1f15d2b077defbdfd4e345c9efb848
nbellila <nbellila@student.42.fr>
2024-10-24 18:41:33 +0200

    print the map

--- srcs/game/draw.c
+++ srcs/game/draw.c
@@ -1,78 +1,40 @@
 #include "cub3d.h"
 
-static void	put_pixel(int x, int y, int color, t_img img)
+void	put_pixel(t_vector pixel, t_img img, int color)
 {
-	char	*pixel;
+	char	*img_pixel;
 	int		offset;
 
-	if (x < 0 || y < 0)
+	if (pixel.x < 0 || pixel.y < 0)
 		return ;
-	offset = (y * img.line_length + x * (img.bits_per_pixel / 8));
-	pixel = img.addr + offset;
-	*(unsigned int *)pixel = color;
+	if (pixel.x >= WINDOW_WIDTH || pixel.y >= WINDOW_HEIGHT)
+		return ;
+	offset = (pixel.y * img.line_length + pixel.x * (img.bits_per_pixel / 8));
+	img_pixel = img.addr + offset;
+	*(unsigned int *)img_pixel = color;
 }
 
-void put_block(t_pos start, int color, t_img img)
+void    put_block(t_data *data, t_vector cell)
 {
-	t_pos	pixel;
-	int x;
-	int y;
-
-	y = -1;
-	while (++y < BLOCK_SIZE)
-	{
-		x = -1;
-		while (++x < BLOCK_SIZE)
-		{
-			pixel.x = start.x + x;
-			pixel.y = start.y + y;
-			if (get_distance(pixel, img.center) > MINIMAP_SIZE)
-				continue;
-			if (x == 0 || y == 0)
-				put_pixel(pixel.x, pixel.y, WHITE, img);
-			else
-				put_pixel(pixel.x, pixel.y, color, img);
-		}
-	}
-}
-
-void put_player(int color, t_img img)
-{
-	int i;
-	int j;
-	
-	i = 0;
-	while (i < PLAYER_SIZE)
-	{
-		j = 0;
-		while (j < PLAYER_SIZE)
-		{
-			put_pixel(img.center.x + j, img.center.y + i, color, img);
-			put_pixel(img.center.x - j, img.center.y + i, color, img);
-			put_pixel(img.center.x + j, img.center.y - i, color, img);
-			put_pixel(img.center.x - j, img.center.y - i, color, img);
-			j++;
-		}
-		i++;
-	}
-}
-
-void put_circle(t_pos pos, int radius, int color, t_img img)
-{
-	const double PI = 3.1415926535;
-	double angle;
-	t_pos	pixel;
-
-	while (radius >= 0)
-	{
-		angle = 0;
-		while (angle < 360)
-		{
-			pixel.x = pos.x + (radius * cos(angle * PI / 180));
-			pixel.y = pos.y + (radius * sin(angle * PI / 180));
-			put_pixel(pixel.x, pixel.y, color, img);
-			angle += 1;
-		}
-		radius--;
-	}
+    t_vector    pixel;
+    t_vector    i;
+    
+    i.y = 0;
+    while (i.y < BLOCK_SIZE)
+    {
+        i.x = 0;
+        while (i.x < BLOCK_SIZE)
+        {
+            pixel.x = (cell.x * BLOCK_SIZE) + i.x;
+            pixel.y = (cell.y * BLOCK_SIZE) + i.y;
+            if (i.x == 0 || i.y == 0)
+                put_pixel(pixel, data->mlx.minimap, WHITE);
+            else if (data->map[cell.y][cell.x] == WALL)
+                put_pixel(pixel, data->mlx.minimap, DARK_BLUE);
+            else if (data->map[cell.y][cell.x] == FLOOR)
+                put_pixel(pixel, data->mlx.minimap, BLUE);
+            i.x++;
+        }
+        i.y++;
+    }
 }
