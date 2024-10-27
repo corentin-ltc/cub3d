af7bf2a5f6b4b9aa45e151c050b7071b1398df2d
Nabil080 <bellilanabil8@gmail.com>
2024-10-28 00:05:52 +0100

    raycasting???

--- srcs/game/raycasting.c
+++ srcs/game/raycasting.c
@@ -2,58 +2,48 @@
 
 static void	get_vertical_intersection(t_ray *ray, t_data *data)
 {
-	ray->step.x = BLOCK_SIZE;
-	ray->step.y = BLOCK_SIZE / tan(ray->angle);
+	ray->step.x = BLOCK_SIZE / BLOCK_SIZE;
+	ray->step.y = BLOCK_SIZE * tan(ray->angle) / BLOCK_SIZE;
 	if (ray->angle > PI / 2 && ray->angle < (3 * PI) / 2)
 	{
-		ray->step.y *= -1;
+		ray->step.x = -1 * BLOCK_SIZE  / BLOCK_SIZE;
+		ray->step.y = -1 * (BLOCK_SIZE * tan(ray->angle))  / BLOCK_SIZE;
 		ray->v_dist.x = floor(ray->start.x) - PX;
 	}
 	else
 		ray->v_dist.x = ceil(ray->start.x) + PX;
 	ray->v_dist.y = ray->start.y + (ray->v_dist.x - ray->start.x) * tan(ray->angle);
-	if (is_wall(data, ray->v_dist.x, ray->v_dist.y))
-	{
-		put_cube(pos(ray->v_dist.x * BLOCK_SIZE, ray->v_dist.y * BLOCK_SIZE), 4, RED, data);
-		return ;
-	}
-	else
-		put_cube(pos(ray->v_dist.x * BLOCK_SIZE, ray->v_dist.y * BLOCK_SIZE), 4, YELLOW, data);
 	while (is_wall(data, ray->v_dist.x, ray->v_dist.y) == false)
 	{
+		// put_cube(pos(ray->v_dist.x * BLOCK_SIZE, ray->v_dist.y * BLOCK_SIZE), 4, YELLOW, data);
 		ray->v_dist.x += ray->step.x;
 		ray->v_dist.y += ray->step.y;
-		printf("v_dist(%lf,%lf)\n", ray->v_dist.x, ray->v_dist.y);
-		put_cube(pos(ray->v_dist.x * BLOCK_SIZE, ray->v_dist.y * BLOCK_SIZE), 4, BLACK, data);
 	}
+	// put_cube(pos(ray->v_dist.x * BLOCK_SIZE, ray->v_dist.y * BLOCK_SIZE), 4, RED, data);
 }
 
 static void	get_horizontal_intersection(t_ray *ray, t_data *data)
 {
-	ray->step.y = BLOCK_SIZE;
-	ray->step.x = BLOCK_SIZE / tan(ray->angle);
 	if (ray->angle < PI && ray->angle > 0)
+	{
+		ray->step.y = BLOCK_SIZE / BLOCK_SIZE;
+		ray->step.x = BLOCK_SIZE / tan(ray->angle)  / BLOCK_SIZE;
 		ray->h_dist.y = ceil(ray->start.y) + PX;
+	}
 	else
 	{
-		ray->step.x *= -1;
+		ray->step.y = -1 * BLOCK_SIZE  / BLOCK_SIZE;
+		ray->step.x = -1 * (BLOCK_SIZE / tan(ray->angle))  / BLOCK_SIZE;
 		ray->h_dist.y = floor(ray->start.y) - PX;
 	}
 	ray->h_dist.x = ray->start.x + (ray->h_dist.y - ray->start.y) / tan(ray->angle);
-	if (is_wall(data, ray->h_dist.x, ray->h_dist.y))
-	{
-		put_cube(pos(ray->h_dist.x * BLOCK_SIZE, ray->h_dist.y * BLOCK_SIZE), 4, RED, data);
-		return ;
-	}
-	else
-		put_cube(pos(ray->h_dist.x * BLOCK_SIZE, ray->h_dist.y * BLOCK_SIZE), 4, PURPLE, data);
 	while (is_wall(data, ray->h_dist.x, ray->h_dist.y) == false)
 	{
-		printf("h_dist(%lf,%lf)\n", ray->h_dist.x, ray->h_dist.y);
+		// put_cube(pos(ray->h_dist.x * BLOCK_SIZE, ray->h_dist.y * BLOCK_SIZE), 4, PURPLE, data);
 		ray->h_dist.x += ray->step.x;
 		ray->h_dist.y += ray->step.y;
-		put_cube(pos(ray->h_dist.x * BLOCK_SIZE, ray->h_dist.y * BLOCK_SIZE), 4, BLACK, data);
 	}
+	// put_cube(pos(ray->h_dist.x * BLOCK_SIZE, ray->h_dist.y * BLOCK_SIZE), 4, RED, data);
 }
 
 void cast_ray(t_data *data, t_pos start, double angle, int color)
@@ -88,8 +78,8 @@ void	raycasting(t_data *data)
 	double	step;
 	size_t	i;
 
-	cast_ray(data, data->player.pos, data->player.angle, WHITE);
-	return ;
+	// cast_ray(data, data->player.pos, data->player.angle, WHITE);
+	// return ;
 	fov = (double)FOV * (PI / 180);
 	angle = data->player.angle - fov / 2;
 	step = fov / WINDOW_WIDTH;
