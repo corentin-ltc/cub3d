ae5728afbe3c3ef4a29814a5d42d7fd3e911716d
Nabil080 <nbellila@student.42.fr>
2024-10-29 20:36:07 +0100

    added border

--- srcs/game/loop.c
+++ srcs/game/loop.c
@@ -9,9 +9,9 @@ static void	sleep_based_on_fps(int fps)
 		usleep(1);
 }
 
-static void	new_img(t_mlx_data mlx, t_img *img)
+static void	new_img(t_mlx_data mlx, t_img *img, int width, int height)
 {
-	img->img = mlx_new_image(mlx.ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	img->img = mlx_new_image(mlx.ptr, width, height);
 	if (!img->img)
 		return ;
 	img->addr = mlx_get_data_addr(img->img,
@@ -22,23 +22,23 @@ static void	new_img(t_mlx_data mlx, t_img *img)
 		return ;
 }
 
-static void	put_img(t_mlx_data mlx, t_img *img)
+static void	put_img(t_mlx_data mlx, t_img *img, int x, int y)
 {
-    mlx_put_image_to_window(mlx.ptr, mlx.win, img->img, 0, 0);
+    mlx_put_image_to_window(mlx.ptr, mlx.win, img->img, x, y);
     mlx_destroy_image(mlx.ptr, img->img);
 	img->img = NULL;
 }
 
 int game_loop(t_data *data)
 {
-	new_img(data->mlx, &data->mlx.minimap);
+	new_img(data->mlx, &data->mlx.minimap, MINIMAP_FULL_SIZE, MINIMAP_FULL_SIZE);
 	process_input(data);
 	rotate_player(data);
 	move_player(data);
-	if (SHOW_MAP && LIGHT)
-    	fill_minimap(data);
 	raycasting(data);
-	put_img(data->mlx, &data->mlx.minimap);
+	if (SHOW_MAP && LIGHT)
+		fill_minimap(data);
+	put_img(data->mlx, &data->mlx.minimap, BORDER_WIDTH, BORDER_WIDTH);
     sleep_based_on_fps(FPS);
     return (0);
 }
